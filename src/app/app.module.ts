ae1cc82293245a6be06cbac45964a00dd63ea523
Moussa-Kalam <m.amzat@alustudent.com>
2023-02-24 04:43:30 +0200

    Product List setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { CartComponent } from './components/cart/cart.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductItemComponent,
+    ProductItemDetailComponent,
+    ConfirmationComponent,
+    CartComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/product-item-detail/product-item-detail.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+
+@Component({
+  selector: 'app-product-item-detail',
+  templateUrl: './product-item-detail.component.html',
+  styleUrls: ['./product-item-detail.component.css']
+})
+export class ProductItemDetailComponent implements OnInit{
+  @Input() product: Product;
+
+  constructor() { 
+    this.product = {
+      id: 1,
+      name: '',
+      price: 1.0,
+      url: '',
+      description: ''
+    }
+  }
+
+  ngOnInit(): void {
+    
+  }
+}
