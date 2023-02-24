e18b3048c4d41ee92dc8d85ce80769964891259d
Moussa-Kalam <m.amzat@alustudent.com>
2023-02-25 00:51:14 +0200

    Routing setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { CartComponent } from './components/cart/cart.component';
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
 
 const routes: Routes = [
-  { path: '', component: ProductListComponent},
-  { path: 'cart', component: CartComponent }
+  { path: '', redirectTo: 'products', pathMatch: 'full'},
+  { path: 'products', component: ProductListComponent},
+  { path: 'products/:id', component: ProductItemDetailComponent},
+  { path: 'cart', component: CartComponent },
+  { path: 'order-success', component: ConfirmationComponent},
+  { path: '**', component: ProductListComponent}
 ];
 
 @NgModule({
